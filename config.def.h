0aee734b79072c09431c56472cbc6769b4b79da4
Ivan Misustin <ivanmis596@gmail.com>
2022-11-07 20:13:36 +0100

    added the uselessgap patch

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/*static const char col_cyan[]        = "#005577";*/
 
 static const char col_cyan[]        = "#344e5b";
 static const char col_green[]       = "#6ac769";
