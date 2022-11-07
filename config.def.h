36871643d8f5fe2dab13272411ed629414b802ff
Ivan Misustin <ivanmis596@gmail.com>
2022-11-07 20:03:07 +0100

    colors and fonts

--- config.def.h
+++ config.def.h
@@ -9,17 +9,22 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 /* static const int user_bh            = 0; */      /* 0 means dwm will calculate bar size */
 /* static const int vertpad            = 12; */
 /* static const int sidepad            = 12; */
-static const char *fonts[]          = { "monospace:size=12" };
+static const char *fonts[]          = { "monospace:size=13" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/*static const char col_cyan[]        = "#005577";*/
+static const char col_cyan[]        = "#344e5b";
+static const char col_green[]       = "#6ac769";
+static const char col_yellow[]      = "#e5c63f";
+static const char col_white[]       = "#f8ecaa";
+static const char col_red[]         = "#e3433f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_white, col_cyan, col_cyan },
+	[SchemeSel]  = { col_cyan, col_red,  col_red},
 };
 
 /* tagging */
