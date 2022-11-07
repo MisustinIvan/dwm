69cdb50c9b4c1f99df04ca16fe0f790440062cff
Ivan Misustin <ivanmis596@gmail.com>
2022-11-07 20:13:03 +0100

    added the uselessgap patch

--- config.def.h
+++ config.def.h
@@ -12,11 +12,11 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=13" };
 static const char dmenufont[]       = "monospace:size=10";
 
-/*static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";*/
+static const char col_cyan[]        = "#005577";
 
 static const char col_cyan[]        = "#344e5b";
 static const char col_green[]       = "#6ac769";
