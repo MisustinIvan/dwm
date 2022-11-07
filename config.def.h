76bbaf0b7ee477381b2864474ac37533273a90d0
Ivan Misustin <ivanmis596@gmail.com>
2022-11-07 21:42:01 +0100

    updated workspace names to greek letters

--- config.def.h
+++ config.def.h
@@ -31,7 +31,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
+static const char *tags[] = { "α", "β", "γ", "δ", "ε", "ζ", "η", "θ", "ι" };
 
 static const Rule rules[] = {
 	/* xprop(1):
