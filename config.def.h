cf3b60885ce00f7b0e51dbdfeac1a4ddf6f26986
Ivan Misustin <ivanmis596@gmail.com>
2022-11-07 07:18:45 +0100

    getting the keybinds correct

--- config.def.h
+++ config.def.h
@@ -1,10 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+/* static const unsigned int gappx     = 12; */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+/* static const int user_bh            = 0; */      /* 0 means dwm will calculate bar size */
+/* static const int vertpad            = 12; */
+/* static const int sidepad            = 12; */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -28,13 +32,12 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -45,7 +48,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,35 +59,68 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+
+#include <X11/XF86keysym.h>
+
+static const char *volumeupcmd[] = { "pulsemixer", "--change-volume", "+5", NULL };
+static const char *volumedowncmd[] = { "pulsemixer", "--change-volume", "-5", NULL };
+
+static const char *brightnessupcmd[] = { "brightnessctl", "set", "+10%", NULL };
+static const char *brightnessdowncmd[] = { "brightnessctl", "set", "-10%", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	/* change the volume */
+	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = volumedowncmd } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volumeupcmd } },
+    /* change the brightness */
+    { 0,                            XF86XK_MonBrightnessUp, spawn, {.v = brightnessupcmd } },
+    { 0,                            XF86XK_MonBrightnessDown, spawn, {.v = brightnessdowncmd } },
+	/* toggle the bar */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	/* focus between the windows */
+	{ MODKEY,                       XK_l,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	/* increment/decrement number of master clients */
+	{ MODKEY,                       XK_s,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	/* increment/decrement the size of the master client */
+	{ MODKEY,                       XK_j,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_semicolon, setmfact,       {.f = +0.05} },
+	/* zoom on a window */
+	{ MODKEY,                       XK_f,      zoom,           {0} },
+	/* i have no idea what this does */
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	/* kill the focused client */
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	/* change to tiling layout */
+	{ MODKEY,                       XK_e,      setlayout,      {.v = &layouts[0]} },
+	/* change to floating layout */
+	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[1]} },
+	/* change to monocle layout */
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[2]} },
+	/* toggle client back to tiling mode */
+	{ MODKEY,                       XK_v,  setlayout,      {0} },
+	/* toggle the client to floating mode */
+	{ MODKEY,                       XK_w,  togglefloating, {0} },
+
+   /* { MODKEY|ShiftMask,             XK_f,  togglefullscr,  {0} }, */
+
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	/* focus the monitor to the left */
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	/* focus the monitor to the right */
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	/* move the client to the monitor to the left */
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	/* move the client to the monitor to the right */
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	/* move clients to workspaces/switch to different workspaces */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -94,7 +130,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* quit the wm */
+	{ MODKEY|ControlMask|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
@@ -113,4 +150,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
