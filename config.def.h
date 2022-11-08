36c21dd72bc6a0a912b1f41f3a0b5b676d8d8d6f
Ivan Misustin <ivanmis596@gmail.com>
2022-11-08 16:07:34 +0100

    rules

--- config.def.h
+++ config.def.h
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    { "processing-app-Base", NULL, NULL, 0, 1, -1 },
+/*    { "processing-app-Base", NULL, NULL, 0, 1, -1 }, */
 };
 
 /* layout(s) */
